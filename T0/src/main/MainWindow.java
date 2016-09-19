6c763a83d706c6cb6626e47ae800cb5e06a3e5f3
Isaac Luiz <isaacluizs@gmail.com>
2016-09-18 21:02:54 -0300

    Alterações

--- T0/src/main/MainWindow.java
+++ T0/src/main/MainWindow.java
@@ -14,6 +14,11 @@ import modelos.Pilha;
 import modelos.Ponto;
 import poo.edgraf.reprodutor.Quadro;
 import reprodutor.CirculoFig;
+import reprodutor.OvalFig;
+import reprodutor.RetaFig;
+import reprodutor.RetanguloFig;
+import reprodutor.TextoFig;
+import t2.bd.CSVWriter;
 
 public class MainWindow {
 
@@ -24,7 +29,11 @@ public class MainWindow {
 	private Ponto ponto1, ponto2;
 
 	private Modelos modelo;
-
+	
+	String textoInformado;
+	
+	CSVWriter writer = new CSVWriter();
+	
 	public MainWindow() {
 		this.initialize();
 	}
@@ -49,16 +58,25 @@ public class MainWindow {
 				MainWindow.this.ponto2 = (MainWindow.this.ponto2 == null) ? new Ponto(e.getX(), e.getY()) : null;
 				switch (MainWindow.this.modelo) {
 				case CIRCULO:
-					CirculoFig fig = new CirculoFig(MainWindow.this.ponto1, 10, panel, Color.BLACK);
-					// fig.reproduzir(new Graphics);
+					CirculoFig circulo = new CirculoFig(MainWindow.this.ponto1, 10, panel, Color.BLACK);
+					
+				//	writer.writeFiguras(figura);
 					break;
 				case OVAL:
+					OvalFig oval = new OvalFig(MainWindow.this.ponto1, ponto1.getX(), ponto2.getX(), panel, Color.BLACK);
+				//	writer.writeFiguras(figura);
 					break;
 				case RETA:
+					RetaFig reta = new RetaFig(ponto1, ponto2, panel, Color.BLACK);
+				//	writer.writeFiguras(figura);
 					break;
 				case RETANGULO:
+					RetanguloFig retangulo = new RetanguloFig(ponto1, ponto2, panel, Color.BLACK);
+				//	writer.writeFiguras(figura);
 					break;
 				case TEXTO:
+					TextoFig texto = new TextoFig(ponto1, textoInformado, panel, Color.BLACK);
+				//	writer.writeFiguras(figura);
 					break;
 				default:
 					break;
@@ -79,18 +97,23 @@ public class MainWindow {
 
 		JMenuItem mntmCirculo = new JMenuItem("Circulo");
 		mntmCirculo.addActionListener(l -> this.modelo = Modelos.CIRCULO);
-		mnModelos.add(mntmCirculo);
+		mnModelos.add(mntmCirculo);		
 
 		JMenuItem mntmOval = new JMenuItem("Oval");
+		mntmOval.addActionListener(l -> this.modelo = Modelos.OVAL);
 		mnModelos.add(mntmOval);
+		
 
 		JMenuItem mntmReta = new JMenuItem("Reta");
+		mntmReta.addActionListener(l -> this.modelo = Modelos.RETA);
 		mnModelos.add(mntmReta);
 
 		JMenuItem mntmRetangulo = new JMenuItem("Retangulo");
+		mntmRetangulo.addActionListener(l -> this.modelo = Modelos.RETANGULO);
 		mnModelos.add(mntmRetangulo);
 
 		JMenuItem mntmTexto = new JMenuItem("Texto");
+		mntmTexto.addActionListener(l -> this.modelo = Modelos.TEXTO);
 		mnModelos.add(mntmTexto);
 	}
 }
