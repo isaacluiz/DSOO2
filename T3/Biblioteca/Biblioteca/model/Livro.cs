08673be0c9f7e0de9048f90b403404401d99d99f
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2016-10-18 11:10:25 -0200

    inicio t3

---
+++ T3/Biblioteca/Biblioteca/model/Livro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.model
+{    
+    class Livro
+    {
+        private String titulo { get; set; }
+
+        private String autor { get; set; }
+
+        private int id { get; set; }
+
+        private DateTime dataEmprestimo { get; set; }
+
+        private DateTime dataDevolucao { get; set; }
+
+    }
+}
